1e323dd802234dd3c783f11ef60a0f9b217bbbb9
FabianMC17 <fabian.maquen@usil.pe>
2023-10-12 23:47:15 -0500

    Universidad agregado

--- S9/Uchill/Universidad.java
+++ S9/Uchill/Universidad.java
@@ -1,8 +1,12 @@
 package Uchill;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Universidad {
     private String nombreU;
     private int sedes;
-    private String ubicacion; 
+    private String ubicacion;
+    private List<Persona> personas; // Lista para almacenar instancias de Persona
     
     Universidad(){
 
@@ -11,9 +15,44 @@ public class Universidad {
         this.nombreU = nombreU;
         this.sedes = sedes;
         this.ubicacion = ubicacion;
+        this.personas = new ArrayList<>();
     }
-    public int nuevaPersona(){
-
+    public String getNombreU() {
+        return nombreU;
+    }
+    public void setNombreU(String nombreU) {
+        this.nombreU = nombreU;
+    }
+    public int getSedes() {
+        return sedes;
+    }
+    public void setSedes(int sedes) {
+        this.sedes = sedes;
+    }
+    public String getUbicacion() {
+        return ubicacion;
+    }
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+    public void setPersonas(List<Persona> personas) {
+        this.personas = personas;
     }
 
+    public void agregarPersona(Persona persona){
+        personas.add(persona);
+    }
+    public void mostrarPersona(){
+        for (Persona persona : personas) {
+            System.out.println(persona);
+        }
+    }
+    @Override
+    public String toString() {
+        return "Universidad [nombreU=" + nombreU + ", sedes=" + sedes + ", ubicacion=" + ubicacion + ", personas="
+                + personas + "]";
+    }
 }
